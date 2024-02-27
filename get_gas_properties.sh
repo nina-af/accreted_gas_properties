b88c8373862533db483c5ba37648aaa126b38874
nina-af <nina.filippova@gmail.com>
2024-02-26 18:13:08 -0600

    Added example bash slurm job submission script for lonestar6.

---
+++ get_gas_properties.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -J M2e3_gas_properties
+#SBATCH -p vm-small
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -t 08:00:00
+#SBATCH --mail-user=ninaf@utexas.edu
+#SBATCH --mail-type=all
+#SBATCH -A AST21048
+
+source $HOME/.bashrc
+source $HOME/miniconda3/etc/profile.d/conda.sh
+
+conda init bash
+conda activate $HOME/miniconda3/envs/py39
+
+./get_simulation_accreted_gas_properties.py 1>get_gas_properties_1.out 2>get_gas_properties_1.err
