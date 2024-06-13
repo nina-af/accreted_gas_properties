11c9c75b7fb9bd3718daa0228843cb5547fabb0f
Nina Filippova <nina_af@login2.frontera.tacc.utexas.edu>
2024-06-13 14:23:19 -0500

    Frontera updates.

--- get_gas_properties.sh
+++ get_gas_properties.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #SBATCH -J get_sink_data
-#SBATCH -p small
+#SBATCH -p development
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -t 12:00:00
+#SBATCH -t 00:20:00
 #SBATCH --mail-user=ninaf@utexas.edu
 #SBATCH --mail-type=all
 #SBATCH -A AST23034
@@ -19,4 +19,4 @@ module unload python3
 
 export QT_QPA_PLATFORM=offscreen
 
-./get_simulation_accreted_gas_properties.py 1>get_gas_properties_batch_0_30_1.out 2>get_gas_properties_batch_0_30_1.err
+./get_simulation_accreted_gas_properties.py 1>get_gas_properties_batch_31_60_1.out 2>get_gas_properties_batch_31_60_1.err
